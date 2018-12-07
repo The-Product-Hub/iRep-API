7221c9f497bf5e6bb7ab792b7c11375b121ee5f2
teevic80 <ogotvictoria@gmail.com>
2018-12-07 04:11:39 +0300

    [ch-models-cleaning] Changed code in model

--- app/models.py
+++ app/models.py
@@ -1,100 +1,11 @@
 from flask import make_response, jsonify, request
 from datetime import date
+from app.v1.views import views
 
 db_records = []
 
 class MyRedflagModels():
-<<<<<<< HEAD
-    def __init__(self):
-        self.db = redflag_records
 
-    def save(self,CreatedBy, IncidenceType, Location, Status, Comment ):
-        data = {
-            "Id" : len(self.db) + 1,
-            "createdOn" : createdOn,
-            "createdBy" : createdBy,
-            "Type": Type,
-            "Location" : Location,
-            "Status" : Status,
-            "Uploads" : Uploads,
-            "Comment" : Comment
-            }
-        self.db.append(data)
-        return self.db
-
-    def post_redflag(self):
-        data = request.get_json()
-        createdOn = datetime['Datetime']
-        createdBy = data['createdBy']
-        Type = data['Type'],
-        Location = data['Location'],
-        Status = data['Status'],
-        Uploads = data['Uploads'],
-        Comment = data['Comment']
-
-        redflag_records.append(post-record)
-        return jsonify{
-            "status":201,
-            "message":"Your Report Has Been Saved Successfully"
-        }  
-
-    def get(self, id):
-        self.id = id
-        response = self.model.find_by_id(self.id)
-        return make_response (jsonify({"My red-flag" : response}), 200)
-        record = [record for record in redflag_records if record['Id'] == id]
-        if len(record) == 0:
-             = jsonify({
-                'status': 404,
-                'message': "No redflag found"
-            })
-
-    def find_by_id(self, id):
-        for redflag in redflags:
-            if redflag['id'] != id: 
-                return {"status": 404,
-                        "message": "Redflag not found"}
-            else:
-                return jsonify({"status": 200,
-                        "Redflag": redflag})
-
-    def patch (self, id):
-        data = request.get_json(force=True)
-        record = [record for record in redflag_records if record['Id'] == id]
-        if len(record) == 0:
-            response = jsonify({
-                'status' : 404,
-                'message' : "No redflag found."
-            })
-
-            response.status_code = 404
-        else:
-            for k in data.keys():
-                if data[k] is not None:
-                    pass
-            
-
-            response = jsonify({
-                'status' : 200,
-                'message' : "redflag updated successfully."
-            })
-
-            response.status_code = 200
-        return response                        
-
-
-    def delete(self, id):
-        data = request.get_json()
-        for redflag in redflags:
-            if redflag['id'] == id:
-                redflags.remove(redflag)
-                return jsonify({"status": 200,
-                        "message": "Redflag has been deleted"})
-            else:
-                return jsonify({"status": 404,
-                        "message": "Redflag does not exist"})        
-=======
-    
     def __init__(self):
         self.db = db_records
 
@@ -120,7 +31,7 @@ class MyRedflagModels():
         if len(db_records) == 0:
             return jsonify({
                 "Status" : 200,
-                "message" : "No Record created"
+                "message" : "No record found."
             })
 
     def get_by_id(self, id):
@@ -156,4 +67,3 @@ class MyRedflagModels():
                     "status" : 404,
                     "message" : "Record does not exist."
                 }) 
->>>>>>> 76749cb4942c9f25808ec9ebf08edbbeb02f92f5
